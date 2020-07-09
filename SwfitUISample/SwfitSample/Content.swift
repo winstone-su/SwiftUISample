242e71b6e8b0e539d08f911fc4a448613296671a
geowin <geowinos@163.com>
2020-07-09 17:41:22 +0800

    Create and Combining Views

---
+++ SwfitUISample/SwfitSample/Content.swift
@@ -0,0 +1,21 @@
+//
+//  Content.swift
+//  SwfitSample
+//
+//  Created by geowin on 2020/6/17.
+//  Copyright © 2020 geowin. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+//struct Content  : View {
+//    @State
+//}
+
+struct Content_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+        
+    }
+}
---
+++ SwfitUISample/SwfitSample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwfitSample
+//
+//  Created by geowin on 2020/6/17.
+//  Copyright © 2020 geowin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var <#name#> = <#value#>
+        
+    }
+
+
+}
+
